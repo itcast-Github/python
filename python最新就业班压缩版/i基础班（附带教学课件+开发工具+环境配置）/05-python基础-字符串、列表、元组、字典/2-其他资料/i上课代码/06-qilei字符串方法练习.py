5d2de6a441c5ac6b5b095c1661a71608e847c2fd
齐磊 <qiilee@126.com>
2018-10-21 09:59:11 +0800

    Update：demo有报错，尚未解决

--- python最新就业班压缩版/i基础班（附带教学课件+开发工具+环境配置）/05-python基础-字符串、列表、元组、字典/2-其他资料/i上课代码/06-qilei字符串方法练习.py
+++ python最新就业班压缩版/i基础班（附带教学课件+开发工具+环境配置）/05-python基础-字符串、列表、元组、字典/2-其他资料/i上课代码/06-qilei字符串方法练习.py
@@ -8,3 +8,9 @@ print("职位：%s"%position)
 print("公司地址：%s"%address)
 print('--------------------------------------------------')
 
+
+userName = input('请输入用户名:')
+print("用户名为：%s"%userName)
+
+password = input('请输入密码:')
+print("密码为：%s"%password))
